e80edda03c4b888aa56e688030751298a826eb5c
shubham16sky <shubhampc16@gmail.com>
2022-11-15 21:54:35 +0530

    added some changes

--- deployment.sh
+++ deployment.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-image=`docker images | grep "airmeet-app"`
-if [ ! -z "$image" ]
-then
-	docker-compose down && docker rmi airmeet-app && docker-compose up -d
-else
-	docker-compose up -d 
-fi
+image=`docker images | grep "airmeet-demo-app"`
+# if [ ! -z "$image" ]
+# then
+# 	docker-compose down && docker rmi airmeet-app && docker-compose up -d
+# else
+# 	docker-compose up -d 
+# fi
+echo $image
 
 
 
